93ce51d4a9db758f5af93e37ecb9da6997ae2756
BrayanAP1 <branalex.0914@gmail.com>
2025-10-06 16:34:39 -0500

    final

--- login/auth.php
+++ login/auth.php
@@ -14,7 +14,7 @@ session_start();
 
 //Verificar si hay sesión activa
 if (!isset($_SESSION['id'])) {
-    header("Location: /index.php");
+    header("Location: /Sotramagdalena/index.php");
     exit();
 }
 
@@ -23,7 +23,7 @@ $tiempo_inactividad = 1800; // 30 minutos
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $tiempo_inactividad)) {
     session_unset();
     session_destroy();
-    header("Location: /index.php?timeout=1");
+    header("Location: /Sotramagdalena/index.php?timeout=1");
     exit();
 }
 $_SESSION['LAST_ACTIVITY'] = time(); // renovar tiempo de actividad
@@ -34,7 +34,7 @@ if (!isset($_SESSION['USER_AGENT'])) {
 } elseif ($_SESSION['USER_AGENT'] !== $_SERVER['HTTP_USER_AGENT']) {
     session_unset();
     session_destroy();
-    header("Location: /index.php");
+    header("Location: /Sotramagdalena/index.php");
     exit();
 }
 ?>
